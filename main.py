f6f0d726416b9832997a931de06599ef437f06b8
shirleyxx <74038718+surelyshirleyxx@users.noreply.github.com>
2022-03-06 09:53:42 -0700

    Adding features

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
-#- Counting words
-#- Counting sentences
-#- Counting lines
-#- Identifying palindromes
-#- Reading the text from a text file
+# - Counting words
+# - Counting sentences
+# - Counting lines
+# - Identifying palindromes
+# - Reading the text from a text file
 
 punc = ".", "!", "?", ":"
-x = input("Enter the text here: ")
 
-list = x.split()
-number_of_words = len(list)
-print(number_of_words)
+x = open('Text_input.txt', 'r')
+content = x.read()
 
-count = len([i for i in x if i in punc])
-print(count)
+# count number of words in the input
+
+word_list = content.split(" ")
+number_of_words = len(word_list)
+print("The number of words in the input is", number_of_words, ".")
+
+# count number of sentences in the input
+
+count = len([i for i in content if i in punc])
+print("The number of sentences in the input is", count, ".")
+
+list_of_lines = content.split("\n")
+number_of_lines = len(list_of_lines)
+print("The number of lines in the input is", number_of_lines, ".")
