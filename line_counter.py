78682b9f5e0e40923ca1a660c8b456f678eea0fb
Y. Eren Bektaş <yusuferenbektas@gmail.com>
2022-03-21 16:00:15 +0300

    Delete line_counter.py

--- line_counter.py
+++
@@ -1,8 +0,0 @@
-def line_count():
-
-    from file_manager import punc, x, content, new, word_list
-
-    list_of_lines = content.split("\n")
-    number_of_lines = len(list_of_lines)
-    print("The number of lines in the input is", number_of_lines, ".")
-    print("\n")
