428ada440ce7bf24fd039eb3913e85bc6f264f4f
Y. Eren Bektaş <yusuferenbektas@gmail.com>
2022-03-21 16:00:24 +0300

    Delete word_counter.py

--- word_counter.py
+++
@@ -1,9 +0,0 @@
-def counter():
-
-    from file_manager import punc, x, content, new, word_list
-
-
-    print(word_list)
-    number_of_words = len(word_list)
-    print("The number of words in the input is", number_of_words, ".")
-    print("\n")
